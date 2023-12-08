c524a7aa159b14cfb66ab765f5001ca851f25842
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-12-07 21:19:06 -0700

    ENH: Add dependencies and update autofill function

--- src/asu-job.py
+++ src/asu-job.py
@@ -1,37 +1,52 @@
 import os
+import re
+import sys
 import time
 
+from zlm import AutoApplyModel
+from multiprocessing import Process
 from playwright.sync_api import Playwright, sync_playwright
 
-ASURITE_PWD = os.environ['ASURITE_PWD']
-SLEEP_TIME = int(os.environ['SLEEP_TIME'])/1000/2
+SLEEP_TIME = 2
+
+def measure_execution_time(func):
+    def wrapper(*args, **kwargs):
+        print(f"Start Function {func.__name__}")
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"END: Function {func.__name__} took {execution_time:.4f} seconds to execute")
+        return result
+
+    return wrapper
 
 def old_click(page, s):
     page.click(s)
     page.wait_for_load_state('load')
 
-def autofill():
-    '''
-    click('text="Apply to job"')
-    click('text="Let\'s get started"')
-    time.sleep(SLEEP_TIME)
-    click('button[id="shownext"]')
-    time.sleep(SLEEP_TIME)
+@measure_execution_time
+def download_resume(job_llm, job_details, user_data):
+    job_llm.resume_builder(job_details, user_data)
 
-    page.click('input[id="radio-44674-No"][type="radio"][value="No"]')
-    page.click('input[id="radio-61829-No"][type="radio"][value="No"]')
-    page.click('span[id="custom_44925_1291_fname_slt_0_44925-button_text"]')
-    page.click('li[role="option"][aria-label="Searching ASU Website"]')
-    click('button[id="shownext"]')
+@measure_execution_time
+def download_cv(job_llm, job_details, user_data):
+    job_llm.cover_letter_generator(job_details, user_data)
 
-    page.click('a[id="AddResumeLink"]')
-    popup = page.wait_for_popup()
-    print(popup)
-    pass
-    '''
+@measure_execution_time
+def auto_apply(job_content: str):
+    job_llm = AutoApplyModel(os.environ['OPENAI_API_KEY'], os.path.abspath('./'))
+    user_data = job_llm.user_data_extraction('src/zlm/demo_data/user_profile.json')
+    job_details = job_llm.job_details_extraction(job_content)
     
-    # =========================================================
-    # Recorded using codegen
+    p_resume = Process(target = download_resume, args=(job_llm, job_details, user_data))
+    p_cv = Process(target = download_cv, args=(job_llm, job_details, user_data))
+    
+    p_resume.start()
+    p_cv.start()
+
+@measure_execution_time
+def autofill(page):
     page.get_by_role("button", name="Apply to job").click()
     page.get_by_role("button", name="Let's get started").click()
     page.get_by_role("button", name="Save and continue").click()
@@ -40,6 +55,7 @@ def autofill():
     page.get_by_role("listbox", name="How did you find out about this job? Choose...").locator("span").first.click()
     page.get_by_role("option", name="Searching ASU Website").get_by_text("Searching ASU Website").click()
     page.get_by_role("button", name="Save and continue").click()
+    p_apply.join()
     page.get_by_role("link", name="requiredRésumé/CV Add résumé/CV").click()
     page.frame_locator("iframe[title=\"Add résumé\\/CV\"]").get_by_role("button", name="Upload a file from Saved résumés/CVs").click()
     page.frame_locator("iframe[title=\"Add résumé\\/CV\"]").get_by_label("Resume_Saurabh_Zinjad+.pdf").check()
@@ -71,36 +87,41 @@ def autofill():
 
     # =========================================================
 
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    try:
-        context = browser.new_context()
+if __name__ == '__main__':
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        try:
+            context = browser.new_context()
 
-        # login
-        page = context.new_page()
-        page.goto('https://weblogin.asu.edu/cas/login?service=https%3A%2F%2Fweblogin.asu.edu%2Fcgi-bin%2Fcas-login%3Fcallapp%3Dhttps%253A%252F%252Fwebapp4.asu.edu%252Fmyasu%252F%253Finit%253Dfalse')
+            # login
+            page = context.new_page()
+            page.goto('https://weblogin.asu.edu/cas/login?service=https%3A%2F%2Fweblogin.asu.edu%2Fcgi-bin%2Fcas-login%3Fcallapp%3Dhttps%253A%252F%252Fwebapp4.asu.edu%252Fmyasu%252F%253Finit%253Dfalse')
 
-        page.fill('input[name=username]', 'szinjad')
-        page.fill('input[name=password]', ASURITE_PWD)
-        page.click('input[type=submit]')
-        page.wait_for_load_state('load')
-        
-        page.get_by_role("link", name="Campus Services").click()
-        page.get_by_role("link", name="Student Employment - Job Search").click()
-        page.get_by_role("button", name="Search On-Campus Jobs").click()
-        page.get_by_role("button", name="Search").click()
-        page.query_selector('#Job_0').click()
+            page.fill('input[name=username]', 'szinjad')
+            page.fill('input[name=password]', os.environ['ASURITE_PWD'])
+            page.click('input[type=submit]')
+            page.wait_for_load_state('load')
+            
+            page.get_by_role("link", name="Campus Services").click()
+            page.get_by_role("link", name="Student Employment - Job Search").click()
+            page.get_by_role("button", name="Search On-Campus Jobs").click()
+            page.get_by_role("button", name="Search").click()
+            time.sleep(SLEEP_TIME)
+            page.query_selector('#Job_0').click()
+            # check already applied
+            while page.get_by_role("button", name="View your applications"):
+                page.get_by_role("link", name="Next Job").click()
+            
+            web_content = re.sub(r'\s+', ' ', page.text_content('.jobDetailsLiner.mainDetails')).strip()
 
-        while True:
-            should_i_autofill = input('Autofill or Exit? (a/e) : ')
+            p_apply = Process(target = auto_apply, args=(web_content,))
+            p_autofill = Process(target = autofill, args=(page,))
+            p_apply.start()
+            p_autofill.start()
 
-            if should_i_autofill == 'a': autofill()
-            elif should_i_autofill == 'e': break
+        except Exception as e:
+            print(e)
 
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        context.close()
-        browser.close()
+        finally:
+            context.close()
+            browser.close()
