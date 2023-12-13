0221ef46d062b6e3b2cc6b3c3061d0622fbe15fd
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-12-12 21:55:24 -0700

    ENH: Refactor code to improve performance and readability

--- src/asu-job.py
+++ src/asu-job.py
@@ -1,11 +1,9 @@
 import os
 import re
-import sys
 import time
 
 from zlm import AutoApplyModel
-from multiprocessing import Process
-from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import sync_playwright
 
 SLEEP_TIME = 2
 
@@ -26,97 +24,133 @@ def old_click(page, s):
     page.wait_for_load_state('load')
 
 @measure_execution_time
-def download_resume(job_llm, job_details, user_data):
-    job_llm.resume_builder(job_details, user_data)
-
-@measure_execution_time
-def download_cv(job_llm, job_details, user_data):
-    job_llm.cover_letter_generator(job_details, user_data)
-
-@measure_execution_time
 def auto_apply(job_content: str):
-    job_llm = AutoApplyModel(os.environ['OPENAI_API_KEY'], os.path.abspath('./'))
+    job_llm = AutoApplyModel(os.environ['OPENAI_API_KEY'], os.path.abspath('./output'))
     user_data = job_llm.user_data_extraction('src/zlm/demo_data/user_profile.json')
     job_details = job_llm.job_details_extraction(job_content)
-    
-    p_resume = Process(target = download_resume, args=(job_llm, job_details, user_data))
-    p_cv = Process(target = download_cv, args=(job_llm, job_details, user_data))
-    
-    p_resume.start()
-    p_cv.start()
+    # job_details = read_json('/home/saurabh/AAA/Convergent/Projects/job-automation/output/SportsProgramsM/SportsProgramsM_JD.json')
 
-@measure_execution_time
-def autofill(webpage: any):
-    webpage.get_by_role("button", name="Apply to job").click()
-    webpage.get_by_role("button", name="Let's get started").click()
-    webpage.get_by_role("button", name="Save and continue").click()
-    webpage.get_by_role("group", name="Are you currently eligible to work in the United States without ASU sponsorship?").get_by_label("Yes").check()
-    webpage.get_by_role("group", name="Are you eligible for Federal Work Study?").get_by_label("Yes").check()
-    webpage.get_by_role("listbox", name="How did you find out about this job? Choose...").locator("span").first.click()
-    webpage.get_by_role("option", name="Searching ASU Website").get_by_text("Searching ASU Website").click()
-    webpage.get_by_role("button", name="Save and continue").click()
-    webpage.get_by_role("link", name="requiredRésumé/CV Add résumé/CV").click()
-    webpage.frame_locator("iframe[title=\"Add résumé\\/CV\"]").get_by_role("button", name="Upload a file from Saved résumés/CVs").click()
-    webpage.frame_locator("iframe[title=\"Add résumé\\/CV\"]").get_by_label("Resume_Saurabh_Zinjad+.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add résumé\\/CV\"]").get_by_role("button", name="Add file").click()
-    webpage.get_by_role("link", name="requiredCover Letter Add cover letter").click()
-    webpage.frame_locator("iframe[title=\"Add Cover Letter\"]").get_by_role("button", name="Upload a file from Saved cover letters").click()
-    webpage.frame_locator("iframe[title=\"Add Cover Letter\"]").get_by_label("Cover_letter_Saurabh_Zinjad+.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add Cover Letter\"]").get_by_role("button", name="Add file").click()
-    webpage.get_by_role("button", name="Save and continue").click()
-    webpage.get_by_role("link", name="Add file").click()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_role("button", name="Upload files from Saved Files").click()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("ASU-SOP.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR turuk maam.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR-Dinesh-Sir.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR-Bansod-Sir.pdf").check()
-    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_role("button", name="Add file").click()
-    webpage.get_by_role("button", name="Save and continue").click()
-    time.sleep(SLEEP_TIME)
-    webpage.get_by_role("button", name="Save and continue").click()
-    time.sleep(SLEEP_TIME)
-    webpage.get_by_role("button", name="Save and continue").click()
-    time.sleep(SLEEP_TIME)
-    webpage.get_by_role("button", name="Save and continue").click()
-    time.sleep(SLEEP_TIME)
-    webpage.get_by_role("button", name="Send my application").click()
-    time.sleep(SLEEP_TIME)
-    webpage.get_by_role("link", name="Job search").click()
-    webpage.get_by_role("button", name="Search").click()
+    cv_path = job_llm.cover_letter_generator(job_details, user_data)
+    resume_path = job_llm.resume_builder(job_details, user_data)
 
-    # =========================================================
+    return cv_path, resume_path
+
+def get_visited_ids() -> set:
+    with open('/home/saurabh/AAA/Convergent/Projects/job-automation/src/visited_job_ids.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+        content = content.strip(',')
+        if content == '':
+            return set()
+        else:
+            return set(content.split(','))
+
+def set_visited_id(id: str) -> set:
+    with open('/home/saurabh/AAA/Convergent/Projects/job-automation/src/visited_job_ids.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{id},')
 
 if __name__ == '__main__':
+    applied_job = 0
+    visited_ids = get_visited_ids()
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         try:
             context = browser.new_context()
 
             # login
-            page = context.new_page()
-            page.goto('https://weblogin.asu.edu/cas/login?service=https%3A%2F%2Fweblogin.asu.edu%2Fcgi-bin%2Fcas-login%3Fcallapp%3Dhttps%253A%252F%252Fwebapp4.asu.edu%252Fmyasu%252F%253Finit%253Dfalse')
+            webpage = context.new_page()
+            webpage.goto('https://weblogin.asu.edu/cas/login')
 
-            page.fill('input[name=username]', 'szinjad')
-            page.fill('input[name=password]', os.environ['ASURITE_PWD'])
-            page.click('input[type=submit]')
-            page.wait_for_load_state('load')
-            
-            page.get_by_role("link", name="Campus Services").click()
-            page.get_by_role("link", name="Student Employment - Job Search").click()
-            page.get_by_role("button", name="Search On-Campus Jobs").click()
-            page.get_by_role("button", name="Search").click()
+            webpage.fill('input[name=username]', 'szinjad')
+            webpage.fill('input[name=password]', os.environ['ASURITE_PWD'])
+            webpage.click('input[type=submit]')
+            webpage.wait_for_load_state('load')
+
+            webpage.goto('https://students.asu.edu/employment/search')
+            webpage.locator('.fa-city').click()
+
+            webpage.get_by_role("button", name="Search").click()
             time.sleep(SLEEP_TIME)
-            page.query_selector('#Job_0').click()
-            # check already applied
-            # while page.get_by_role("button", name="View your applications"):
-            #     page.get_by_role("link", name="Next Job").click()
-            
-            web_content = re.sub(r'\s+', ' ', page.text_content('.jobDetailsLiner.mainDetails')).strip()
 
-            p_apply = Process(target = auto_apply, args=(web_content,))
-            p_autofill = Process(target = autofill, args=(page,))
-            p_apply.start()
-            p_autofill.start()
+            while True:
+                try:
+                    list_locator = webpage.locator('li.job.baseColorPalette.ng-scope')
+                    count = list_locator.count()
+
+                    is_link_clicked = False
+                    for i in range(count):
+                        job_id = list_locator.nth(i).locator('p.jobProperty.position3').last.text_content()
+                        if job_id not in visited_ids:
+                            webpage.query_selector(f'#Job_{i}').click()
+                            is_link_clicked = True
+                            time.sleep(SLEEP_TIME)
+                            break
+                    
+                    if not is_link_clicked:
+                        webpage.locator('.showMoreJobs').last.click()
+                        continue
+
+                    while webpage.get_by_role("button", name="View your applications").is_visible():
+                        job_id = webpage.locator('p[class="answer ng-scope position3InJobDetails"]').last.text_content()
+                        if job_id.strip() != '':
+                            set_visited_id(job_id)
+                            visited_ids.add(job_id)
+                        webpage.get_by_role("link", name="Next Job").last.click()
+                        time.sleep(SLEEP_TIME)
+                    
+                    # page_next = webpage.get_by_role("link", name="Next Job")
+                    time.sleep(SLEEP_TIME)
+                    web_content = re.sub(r'\s+', ' ', webpage.text_content('.jobDetailsLiner.mainDetails')).strip()
+                    print(f"\n========================\n {applied_job+1} : {web_content[:75]}\n========================\n\n")
+
+                    cv_path, resume_path = auto_apply(web_content)
+
+                    webpage.get_by_role("button", name="Apply to job").click()
+                    webpage.get_by_role("button", name="Let's get started").click()
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    webpage.get_by_role("group", name="Are you currently eligible to work in the United States without ASU sponsorship?").get_by_label("Yes").check()
+                    webpage.get_by_role("group", name="Are you eligible for Federal Work Study?").get_by_label("Yes").check()
+                    webpage.get_by_role("listbox", name="How did you find out about this job? Choose...").locator("span").first.click()
+                    webpage.get_by_role("option", name="Searching ASU Website").get_by_text("Searching ASU Website").click()
+                    webpage.get_by_role("button", name="Save and continue").click()
+
+                    webpage.get_by_role("link", name="requiredRésumé/CV Add résumé/CV").click()
+                    webpage.frame_locator("#profileBuilder").get_by_label("Browse to upload a file from local storage").set_input_files(resume_path)
+                    time.sleep(SLEEP_TIME)
+                    
+                    webpage.get_by_role("link", name="requiredCover Letter Add cover letter").click()
+                    webpage.frame_locator("#profileBuilder").get_by_label("Browse to upload a file from local storage").set_input_files(cv_path)
+                    time.sleep(SLEEP_TIME)
+
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    webpage.get_by_role("link", name="Add file").click()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_role("button", name="Upload files from Saved Files").click()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("ASU-SOP.pdf").check()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR turuk maam.pdf").check()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR-Dinesh-Sir.pdf").check()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_label("LOR-Bansod-Sir.pdf").check()
+                    webpage.frame_locator("iframe[title=\"Add documents for Supporting Documentation\"]").get_by_role("button", name="Add file").click()
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    time.sleep(SLEEP_TIME)
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    time.sleep(SLEEP_TIME)
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    time.sleep(SLEEP_TIME)
+                    webpage.get_by_role("button", name="Save and continue").click()
+                    time.sleep(SLEEP_TIME)
+                    webpage.get_by_role("button", name="Send my application").click()
+                    time.sleep(SLEEP_TIME)
+                    # page_next.click()
+                    webpage.get_by_role("link", name="Job search").click()
+                    webpage.get_by_role("button", name="Search").click()
+                    time.sleep(SLEEP_TIME)
+                    applied_job += 1
+                    if job_id.strip() != '':
+                        set_visited_id(job_id)
+                        visited_ids.add(job_id)
+                    
+                except Exception as e:
+                    print(e)
+                    break
 
         except Exception as e:
             print(e)
